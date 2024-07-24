710625ddeac9a9f18a1b42e30a0b5a595e4d2775
Milanavika <v.novak@nv.dunice.net>
2024-07-24 09:11:22 +0300

    edits1

--- algoritmic/1/1.js
+++ algoritmic/1/1.js
@@ -2,48 +2,40 @@
 // на каждый эндпойнт, передавая в каждом последующем запросе
 // результат ответа предыдущего запроса. Какими способами можно 
 // это сделать в JS?
-const endPoints = [
-  'https://jsonplaceholder.typicode.com/users?name=Leanne%20Graham',
-  'https://jsonplaceholder.typicode.com/todos?user=*V2',
-  'https://jsonplaceholder.typicode.com/todos/*V3'
-   ]
 
    // *V2 - тут id пользователя полученного в прошлом запросе.
    // *V3 - тут id последней таски, полученной во втором запросе
+   async function fetchData(url){
+    const response = await fetch(url);
+    if(response.status !== 200){
+      throw new Error('err')
+    }
+    return response.json();
+   }
 
- function fetchRequest(endpoins){
-  let result;
-  let promise = Promise.resolve();
-  endpoins.forEach((endpoint, index) =>{
-    promise=promise
-    .then(() => {
-      if(index === 1 && result){
-        endpoint=endpoint.replace('*V2', result[0].id)
-      }
-      if(index === 2 && result){
-        endpoint=endpoint.replace('*V3', result[0].id)
-      }
-      return fetch(endpoint, {
-        method:'POST',
-        headers: {
-          'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(result)
-      })
-    })
-    .then(response => {
-      if(!response.ok){
-        throw new Error('error')
-      }
-      return response.json()
-    })
-    .then(data => {
-      result=data;
-      return result;
-    })
-    .catch(error=>{
+ async function fetchRequest(){
+  const endPoints = [
+    'https://jsonplaceholder.typicode.com/users?name=Leanne%20Graham',
+    'https://jsonplaceholder.typicode.com/todos?user=*V2',
+    'https://jsonplaceholder.typicode.com/todos/*V3'
+    ];
+
+     try{
+      const result = await fetchData(endPoints[0]);
+      const userId = result[0].id;
+
+      const todoUrl = endPoints[1].replace('*V2', userId)
+      const todos = await fetchData(todoUrl)
+      const lastTaskId = todos[todos.length-1].id;
+
+      const lastTasckUrl = endPoints[2].replace('*V3', lastTaskId)
+      const lastTasckUrlDetails = await fetchData(lastTasckUrl)
+
+      console.log('User:', result[0]);
+      console.log('UserId:', userId);
+      console.log('Todos:', lastTaskId);
+     } catch( error){
       console.error('error', error)
-    })
-  })
-  return promise
-}
+     }
+  }
+  fetchRequest();
